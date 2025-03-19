8bbc4e3315d194fdca6ba8cd8d3f8f81900e5e19
pylearning9512 <pylearning9512@gmail.com>
2025-03-18 23:41:13 -0600

    Delete coding directory

--- coding/Coding/Py projects/Python/data analytics/Loop, range and reverse practice.py
+++
@@ -1,6 +0,0 @@
-string= input("enter your text here: ")
-repeat= input("enter the number of times you want to repeat the text: ")
-def repeat_str(repeat, string):
-    print(string*int(repeat))
-
-repeat_str(repeat, string)
